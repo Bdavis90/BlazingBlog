3459f3c937977c504041eb7e79e2b8331273b1c6
Brandon Davis <wowzerss90@yahoo.com>
2025-01-19 16:55:11 -0500

    Added Identity package. Created custom identity user.

---
+++ BlazingBlogDomain/Users/IUser.cs
@@ -0,0 +1,17 @@
+﻿using BlazingBlogDomain.Articles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingBlogDomain.Users
+{
+    public interface IUser
+    {
+        public string Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public List<Article> Articles { get; set; }
+    }
+}
---
+++ BlazingBlogInfastructure/Authentication/User.cs
@@ -0,0 +1,16 @@
+﻿using BlazingBlogDomain.Articles;
+using BlazingBlogDomain.Users;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingBlogInfastructure.Authentication
+{
+    public class User : IdentityUser, IUser
+    {
+        public List<Article> Articles { get; set; } = new List<Article>();
+    }
+}
