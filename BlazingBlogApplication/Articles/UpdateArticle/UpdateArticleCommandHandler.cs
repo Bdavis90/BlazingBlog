7dda842967057a4d86a2624cbca0122e5e335616
Brandon Davis <wowzerss90@yahoo.com>
2025-02-09 20:55:25 -0500

    Updated UpdateCommand to user UserService

--- BlazingBlogApplication/Articles/UpdateArticle/UpdateArticleCommandHandler.cs
+++ BlazingBlogApplication/Articles/UpdateArticle/UpdateArticleCommandHandler.cs
@@ -1,17 +1,25 @@
-﻿namespace BlazingBlogApplication.Articles.UpdateArticle
+﻿using BlazingBlogApplication.Users;
+
+namespace BlazingBlogApplication.Articles.UpdateArticle
 {
     internal class UpdateArticleCommandHandler : ICommandHandler<UpdateArticleCommand, ArticleResponse?>
     {
         private readonly IArticleRepository _articleRepository;
+        private readonly IUserService _userService;
 
-        public UpdateArticleCommandHandler(IArticleRepository articleRepository)
+        public UpdateArticleCommandHandler(IArticleRepository articleRepository, IUserService userService)
         {
             _articleRepository = articleRepository;
+            _userService = userService;
         }
 
         public async Task<Result<ArticleResponse?>> Handle(UpdateArticleCommand request, CancellationToken cancellationToken)
         {
             var updatedArticle = request.Adapt<Article>();
+            if(!await _userService.CurrentUserCanEditArticleAsync(updatedArticle.Id))
+            {
+                return Result.Fail<ArticleResponse?>("You're not allowed to edit this article! How did you get here.");
+            }
             var article =  await _articleRepository.UpdateArticleAsync(updatedArticle);
             if(article is null)
             {
