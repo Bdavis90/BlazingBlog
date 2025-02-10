228fcc2ca6cf1f7044e6c170408476cf255c28aa
Brandon Davis <wowzerss90@yahoo.com>
2025-02-09 20:48:27 -0500

    Used the new UserService to get the userid with the create article command handler.

--- BlazingBlogApplication/Articles/CreateArticle/CreateArticleCommandHandler.cs
+++ BlazingBlogApplication/Articles/CreateArticle/CreateArticleCommandHandler.cs
@@ -1,24 +1,45 @@
 ﻿
 
 
+using BlazingBlogApplication.Exceptions;
+using BlazingBlogApplication.Users;
+
 namespace BlazingBlogApplication.Articles.CreateArticle
 {
     public class CreateArticleCommandHandler : ICommandHandler<CreateArticleCommand, ArticleResponse>
     {
         private readonly IArticleRepository _articleRepository;
+        private readonly IUserService _userService;
 
-        public CreateArticleCommandHandler(IArticleRepository articleRepository)
+        public CreateArticleCommandHandler(IArticleRepository articleRepository, IUserService userService)
         {
             _articleRepository = articleRepository;
+            _userService = userService;
         }
 
         public async Task<Result<ArticleResponse>> Handle(CreateArticleCommand request, CancellationToken cancellationToken)
         {
-            var newArticle = request.Adapt<Article>();
-            var article = await _articleRepository.CreateArticleAsync(newArticle);
-            return article.Adapt<ArticleResponse>();
+            try
+            {
+                var newArticle = request.Adapt<Article>();
+                newArticle.UserId = await _userService.GetCurrentUserIdAsync();
+
+                if (!await _userService.CurrentUserCanCreateArtilcesAsync())
+                {
+                    return FailingResult();
+                }
+                var article = await _articleRepository.CreateArticleAsync(newArticle);
+                return article.Adapt<ArticleResponse>();
+            }
+            catch (UserNotAuthorizedException) 
+            {
+                return FailingResult();
+            }
         }
 
- 
+        private Result<ArticleResponse> FailingResult()
+        {
+            return Result.Fail<ArticleResponse>("You're not allowed to create articles. Sorry!");
+        }
     }
 }
